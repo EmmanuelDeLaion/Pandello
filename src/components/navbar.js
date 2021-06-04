30c25909628df8ce579e690ead30c72a6147dfc1
Mono <Mono@DESKTOP-BR5IUL7>
2021-06-04 17:12:49 -0500

    navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+
+                <nav class="navbar navbar-light bg-light">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">
+                            {this.props.titulo}
+                        </a>
+                    </div>
+                </nav>
+
+            </div>
+        )
+    }
+}
+
+export default Navbar;
