6cac5c2194ec0471bb0c7b554f97addc62816764
Mono <Mono@DESKTOP-BR5IUL7>
2021-06-05 02:27:01 -0500

    cambiar titulo de columnas

--- src/components/TituloLista.js
+++ src/components/TituloLista.js
@@ -1,15 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import ContextApi from '../ContextApi';
 
-const TituloLista = ({titulo, IdLista}) => {
+const TituloLista = ({ titulo, idListas }) => {
 
     const [tituloLista, setTituloLista] = useState(titulo);
+    const { actualizarTituloLista } = useContext(ContextApi);
+
+
+    const handleBlur = () => {
+        actualizarTituloLista(tituloLista, idListas);
+    }
+
 
     return (
         <div>
 
             <form>
                 <div className="form-group">
-                    <input type="text" className="form-control titulo-input" value={tituloLista} onChange={e => setTituloLista(e.target.value)} ></input>
+                    <input
+                        onBlur={handleBlur}
+                        type="text"
+                        className="form-control titulo-input"
+                        value={tituloLista}
+                        onChange={e => setTituloLista(e.target.value)} >
+                    </input>
                 </div>
             </form>
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+ 
 class Navbar extends Component {
     render() {
         return (
